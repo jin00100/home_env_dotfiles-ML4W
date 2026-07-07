477a59b0d7d0da4ddc9abfb39019319fba61f65f
jin <jin10190220@gmail.com>
2026-07-08 00:07:50 +0900

    update kitty

--- nix/modules/desktop/assets/hypr/conf/windowrules/default.lua
+++ nix/modules/desktop/assets/hypr/conf/windowrules/default.lua
@@ -3,7 +3,9 @@ hl.config({
         "noanim, class:^(fcitx)$",
         "noanim, class:^(fcitx5)$",
         "noborder, class:^(fcitx5)$",
-        "noshadow, class:^(fcitx5)$"
+        "noshadow, class:^(fcitx5)$",
+        "opacity 0.65 0.65, class:^(google-chrome)$",
+        "opacity 0.65 0.65, class:^(Google-chrome)$"
     },
     layerrule = {
         "noanim, fcitx",
